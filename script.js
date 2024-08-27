1c7978affdc58ac012750740573b615e2b83aea7
filipsjostrand <filip_sjostrand@hotmail.com>
2024-08-27 18:42:45 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// const arrowUp = document.getElementById("fontawesome-arrow-up");
+// const arrowDown = document.getElementById("fontawesome-arrow-down");
+
+
+//     window.addEventListener("scroll", (event) => {
+//         myFunction();
+//     });
+
+//   function myFunction() {
+
+//     // Reset the animation
+//     arrowUp.style.animation = "none";
+//     arrowUp.offsetHeight; // Trigger a reflow
+//     arrowDown.style.animation = "none";
+//     arrowDown.offsetHeight;
+
+//     arrowUp.style.animation = "show-arrows 2.5s linear";
+//     arrowDown.style.animation = "show-arrows 2.5s linear";
+//   }
